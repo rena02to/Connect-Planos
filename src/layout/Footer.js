5d77291effed7294a1732e556f44b5b09fe00466
Renato Alves <108096649+rena02to@users.noreply.github.com>
2023-11-08 13:33:18 -0300

    Update Footer.js

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <footer className={style.footer}>
             <div>
-                <a href="https://linkedin.com/in/rena02to/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                <a href="https://linkedin.com/in/renatosalves/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
                 <a href="https://github.com/rena02to" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
             </div>
             <p>Connect Planos &copy; 2023</p>
